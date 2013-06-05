e725cd9a792a839e0cc0598c782604404e4667d2
Adam Moses <adammoses13@hotmail.com>
2013-06-04 19:26:52 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaddysSite::Application.config.secret_token = '76c0d3d492393b9f0a6ba8667bdd315c059f062265dbac63e905cded4d5616fd5a7626fbdf3bb3fd7e2f5a4fa0cee42ca07aa56688492be5a8e965576f1285bf'
