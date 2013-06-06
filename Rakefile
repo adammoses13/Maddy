9c3327da649030b488e87caaa5b28f37a0d7f51c
Adam Moses <adammoses13@hotmail.com>
2013-06-05 22:29:09 -0600

    Update

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-MaddysSite::Application.load_tasks
+Maddysmusic::Application.load_tasks
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-MaddysSite::Application.config.session_store :cookie_store, key: '_MaddysSite_session'
+Maddysmusic::Application.config.session_store :cookie_store, key: '_MaddysSite_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# MaddysSite::Application.config.session_store :active_record_store
+# Maddysmusic::Application.config.session_store :active_record_store
