44e23a59417f27e807cb3b60053039ea2e4ec5ce
Adam Moses <adammoses13@hotmail.com>
2013-06-06 12:32:32 -0600

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Maddysmusic::Application.routes.draw do
-  get 'About' => 'pages#About'
+  get 'About_Me' => 'pages#About_Me'
+  get 'My_Philosophy' => 'pages#My_Philosophy'
+  get 'Contact_Me' => 'pages#Contact_Me'
+  get 'Resources' => 'pages#Resources'
+  root :to => 'pages#home'
 
-  get 'Philosophy' => 'pages#Philosophy'
 
- root :to => 'pages#home'
+
+ 
 end
