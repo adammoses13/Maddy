558e6757fe570e652ab9c5a316327a63341c18d4
Adam Moses <adammoses13@hotmail.com>
2013-06-12 16:51:51 -0600

    Update Formatting

---
+++ app/models/message.rb
@@ -0,0 +1,33 @@
+class Message
+    # Remove the inheritance from ActiveRecord::Base (class Message < ActiveRecord::Base)
+
+    # Include ActiveAttr functionality
+    include ActiveAttr::Model
+
+    # Include ActiveModel::Validations
+    include ActiveModel::Validations
+
+    # Define attributes
+    attribute :name
+    attribute :email
+    attribute :phone
+    attribute :body
+
+    # Mass assignment security
+    # Whitelist attributes that you want to mass assign user given data to
+    attr_accessible :name, :email, :phone, :body
+
+    # Validations
+    # Name must be present
+    validates_presence_of :name
+
+    # Email must be present and valid email format
+    validates_presence_of :email
+    validates :email, email_format: { message: "is not looking like a valid email address"}
+
+    # Phone must be present
+    validates_presence_of :phone
+
+    # Body is optional but if given must be 500 characters at maximum
+    validates_length_of :body, maximum: 500
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Maddysmusic::Application.routes.draw do
+  
   get 'About_Me' => 'pages#About_Me'
   get 'My_Philosophy' => 'pages#My_Philosophy'
   get 'Contact_Me' => 'pages#Contact_Me'
   get 'Resources' => 'pages#Resources'
+  
   root :to => 'pages#home'
 
 
