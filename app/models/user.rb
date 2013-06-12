36fe356fb05dcae448d47d073f15074d2d5219b6
Adam Moses <adammoses13@hotmail.com>
2013-06-12 17:52:12 -0600

    Cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :login, :name
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, #:recoverable,
+         :rememberable, :trackable, :validatable
+
+
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 end
