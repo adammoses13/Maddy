10630c1410ad6df41f2ba3d5bae4d1f619649d0c
Adam Moses <adammoses13@hotmail.com>
2013-07-23 22:16:56 -0600

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Maddysmusic::Application.routes.draw do
-  
+    
+ 
+	
+	get 'Resources' => 'pages#Resources'
   get 'About_Me' => 'pages#About_Me'
   get 'My_Philosophy' => 'pages#My_Philosophy'
   get 'Contact_Me' => 'pages#Contact_Me'
-  get 'Resources' => 'pages#Resources'
- 
+  
+  devise_for :users
   root :to => 'pages#home'
 
 
