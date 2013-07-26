58f4b69851928f4c2595fdbbd4cd464b4cd5d17c
Adam Moses <adammoses13@hotmail.com>
2013-07-25 23:16:41 -0600

    Add Student's Area

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, #:recoverable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable, :authentication_keys => [:name]
+ 
 
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+
+	validates_presence_of :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Maddysmusic::Application.routes.draw do
   get 'About_Me' => 'pages#About_Me'
   get 'My_Philosophy' => 'pages#My_Philosophy'
   get 'Contact_Me' => 'pages#Contact_Me'
+  get 'For_Students' => 'pages#For_Students'
   
   devise_for :users
   root :to => 'pages#home'
