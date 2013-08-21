4da7058541013cb9e3a9191815e4c8426b3d42fd
Adam Moses <adammoses13@hotmail.com>
2013-08-21 15:43:20 -0600

    Add Admin to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130821190852) do
+ActiveRecord::Schema.define(:version => 20130821212832) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -26,10 +26,10 @@ ActiveRecord::Schema.define(:version => 20130821190852) do
   end
 
   create_table "users", :force => true do |t|
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20130821190852) do
     t.string   "last_sign_in_ip"
     t.string   "name"
     t.string   "displayname"
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
