fe2abb1c4e8f73dce413fd655015e485f8e734ec
Adam Moses <adammoses13@hotmail.com>
2013-08-22 14:53:12 -0600

    make change to new student's area

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource_or_scope)
-    For_Students_path(@pages)
+    comments_path(@comments)
 end
 end
