78da7dd696aaa2c4f2ec3abf394e7dd68dc3002f
Adam Moses <adammoses13@hotmail.com>
2013-08-28 14:17:13 -0600

    change number of reviews showing

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   # GET /reviews
   # GET /reviews.json
   def index
-    @reviews = Review.order("created_at desc").page(params[:page]).per_page(2)
+    @reviews = Review.order("created_at desc").page(params[:page]).per_page(10)
 
     respond_to do |format|
       format.html # index.html.erb
