91faf6cd3d8abe5bb35a9afbb08b0f227274200e
Adam Moses <adammoses13@hotmail.com>
2014-03-16 23:50:36 -0600

    debug audio challenge

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,7 @@ class Comment < ActiveRecord::Base
   					:url => "/system/:attachment/:id/:style/:filename"
   has_attached_file :doc
             
-  validates_attachment_content_type :audio, :content_type => ['audio/mp3', 'audio/mp4', 'application/mp3', 'audio/mpeg']
+  validates_attachment_content_type :audio, :content_type => ['audio/mp3', 'audio/mp4', 'application/mp3', 'audio/mpeg','audio/MP3']
   validates_attachment_content_type :doc, :content_type => ['application/msword', 'application/pdf', "application/vnd.openxmlformats-officedocument.wordprocessingml.document", 
              "text/plain"] 
   
