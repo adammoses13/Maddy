78c6afe0320bc07539895e92b8085c8178c182d0
Adam Moses <adammoses13@hotmail.com>
2014-07-13 20:38:42 -0600

    update honeypot

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,12 +2,15 @@ class Review < ActiveRecord::Base
    attr_accessible :name, :review, :content, :check
 
 validate :content_is_blank
+validate :check_is_blank
 
 def content_is_blank
   errors.add :content if content.present?
 end
 
-
+def check_is_blank
+  errors.add :check if check.present?
+end
 
 def send_review_notification
   UserMailer.review_notification(self).deliver
